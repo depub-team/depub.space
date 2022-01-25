84bd463b42497766b5d0f0a975dc94babfacdf11
0xtaipoian <0xtaipoian@protonmail.com>
2022-01-25 15:57:01 +0800

    feat(depub.space): support hyperlinks

--- packages/depub.space/src/pages/_document.tsx
+++ packages/depub.space/src/pages/_document.tsx
@@ -1,38 +1,8 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import React, { Children } from 'react';
 import { AppRegistry } from 'react-native';
+import { globals } from '@depub/theme';
 import config from '../../app.json';
-
-// Force Next-generated DOM elements to fill their parent's height
-const normalizeNextElements = `
-  body, html {
-    width: 100%;
-    min-height: 100%;
-    padding: 0;
-    margin: 0;
-  }
-
-  html {
-    scroll-behavior: smooth;
-    -webkit-text-size-adjust: 100%;
-    height: calc(100% + env(safe-area-inset-top));
-  }
-
-  body {
-    display: flex;
-    overflow-y: auto;
-    overscroll-behavior-y: none;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-  }
-
-  #__next {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    width: 100%;
-  }
-`;
 
 export default class MyDocument extends Document {
   public static async getInitialProps({ renderPage }: DocumentContext) {
@@ -44,7 +14,7 @@ export default class MyDocument extends Document {
     const page = await renderPage();
     const styles = [
       // eslint-disable-next-line react/jsx-key, react/no-danger
-      <style dangerouslySetInnerHTML={{ __html: normalizeNextElements }} />,
+      <style dangerouslySetInnerHTML={{ __html: globals }} />,
       getStyleElement(),
     ];
 
