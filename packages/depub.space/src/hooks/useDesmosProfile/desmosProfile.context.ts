c39d54c698e1098957bb9614c08bf6a2cab82375
0xtaipoian <0xtaipoian@protonmail.com>
2022-01-27 08:12:27 +0800

    feat(depub.space): add desmos profile and link preview

--- packages/depub.space/src/hooks/useDesmosProfile/desmosProfile.context.ts
+++
@@ -1,9 +0,0 @@
-import { DesmosProfile } from '@desmoslabs/sdk-core';
-import { createContext } from 'react';
-
-export interface DesmosProfileContextProps {
-  cachedProfiles?: Record<string, DesmosProfile | null>;
-  setCachedProfiles?: React.Dispatch<React.SetStateAction<Record<string, DesmosProfile | null>>>;
-}
-
-export const DesmosProfileContext = createContext<DesmosProfileContextProps>({});
