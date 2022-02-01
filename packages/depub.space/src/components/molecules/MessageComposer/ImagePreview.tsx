cd723566782482bd8c3fe11114948e7331054f00
0xtaipoian <0xtaipoian@protonmail.com>
2022-02-01 14:42:21 +0800

    chore: updae image preview

--- packages/depub.space/src/components/molecules/MessageComposer/ImagePreview.tsx
+++ packages/depub.space/src/components/molecules/MessageComposer/ImagePreview.tsx
@@ -1,15 +1,25 @@
 import React, { FC } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
-import { Box, HStack, Icon, Image, Pressable, Tooltip } from 'native-base';
+import { AspectRatio, Box, HStack, Icon, Image, Pressable, Tooltip } from 'native-base';
 
 export const ImagePreview: FC<{ image: ImagePicker.ImageInfo; onRemoveImage: () => void }> = ({
   image,
   onRemoveImage,
 }) => (
-  <HStack pb={4} px={4} space={4}>
-    <Box key={image.uri} position="relative">
-      <Image alt="upload image" resizeMode="cover" size="2xl" source={{ uri: image.uri }} />
+  <HStack pb={4} space={4}>
+    <Box key={image.uri} borderRadius="sm" flex={1} position="relative">
+      <AspectRatio ratio={1}>
+        <Image
+          alt="Upload image"
+          borderColor="gray.200"
+          borderRadius="md"
+          borderWidth={1}
+          resizeMode="cover"
+          source={{ uri: image.uri }}
+          textAlign="center"
+        />
+      </AspectRatio>
       <Tooltip label="Remove" openDelay={250}>
         <Pressable position="absolute" right="0" top="0" onPress={onRemoveImage}>
           <Icon
