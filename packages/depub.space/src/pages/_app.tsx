bfd77b26a50cc211005ac9a15c510a1467832a88
0xtaipoian <0xtaipoian@protonmail.com>
2022-02-08 09:54:42 +0800

    fix: address display

--- packages/depub.space/src/pages/_app.tsx
+++ packages/depub.space/src/pages/_app.tsx
@@ -13,12 +13,18 @@ function MyApp({ Component, pageProps }: AppProps) {
     // eslint-disable-next-line func-names
     void (async function () {
       /* eslint-disable global-require, @typescript-eslint/no-var-requires */
-      void (await Font.loadAsync({
-        Inter: require('../../public/fonts/Inter-Regular.ttf').default,
-      }));
-      void (await Font.loadAsync({
-        Inter_bold: require('../../public/fonts/Inter-Bold.ttf').default,
-      }));
+      try {
+        void (await Promise.all([
+          Font.loadAsync({
+            Inter: require('../../public/fonts/Inter-Regular.ttf').default,
+          }),
+          Font.loadAsync({
+            Inter_bold: require('../../public/fonts/Inter-Bold.ttf').default,
+          }),
+        ]));
+      } catch (e) {
+        // FontObserver timeout after 3000ms, this will fail silently
+      }
       /* eslint-enable global-require, @typescript-eslint/no-var-requires */
 
       setIsLoading(false);
