85668aceb33e484aa1699e95d6b1fd483ea15abe
0xtaipoian <0xtaipoian@protonmail.com>
2022-02-23 20:12:36 +0800

    fix: aspect ratio in modal

--- packages/depub.space/src/components/molecules/ImageModal/ImageModal.tsx
+++ packages/depub.space/src/components/molecules/ImageModal/ImageModal.tsx
@@ -11,9 +11,12 @@ export const ImageModal: FC<ImageModalProps> = ({ source, aspectRatio = 1, ...pr
     <Modal.Content maxH="100%" maxW={1000} w="90%">
       <Modal.CloseButton />
       <Modal.Body p={0}>
-        <AspectRatio ratio={aspectRatio}>
-          <Image alt="photo" flex={1} size="full" source={source} />
-        </AspectRatio>
+        {/* XXX: aspect ratio doesnt work in modal when second open, here force it re-render */}
+        {props.isOpen ? (
+          <AspectRatio h="100%" ratio={aspectRatio} w="100%">
+            <Image alt="photo" flex={1} size="full" source={source} />
+          </AspectRatio>
+        ) : null}
       </Modal.Body>
     </Modal.Content>
   </Modal>
