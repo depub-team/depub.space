c9d8559e2bfd41473d1fed440825f30cd5b68fed
0xtaipoian <0xtaipoian@protonmail.com>
2022-02-26 18:24:35 +0800

    chore: update deps

--- packages/depub.space/.eslintrc.js
+++ packages/depub.space/.eslintrc.js
@@ -5,13 +5,13 @@ module.exports = {
       parserOptions: {
         project: ['./tsconfig.json'], // Specify it only for TypeScript files
       },
+      rules: {
+        '@next/next/no-html-link-for-pages': ['error', 'packages/depub.space/src/pages']
+      }
     },
   ],
   extends: [
     'plugin:@next/next/recommended',
     '../../.eslintrc.base.json'
   ],
-  rules: {
-    '@next/next/no-html-link-for-pages': ['error', 'packages/depub.space/src/pages']
-  }
 }
