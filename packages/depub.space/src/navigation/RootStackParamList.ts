7dbfc63997454e9d66c5f7bc8346f94f7e0ab635
0xtaipoian <0xtaipoian@protonmail.com>
2022-03-15 10:17:29 +0800

    fix(depub.space): routing

--- packages/depub.space/src/navigation/RootStackParamList.ts
+++ packages/depub.space/src/navigation/RootStackParamList.ts
@@ -1,11 +1,6 @@
 export type RootStackParamList = {
   Main: undefined;
   NotFound: undefined;
-  ConnectWallet: undefined;
-  Image: {
-    image: string;
-    aspectRatio?: number;
-  };
   Post: {
     id: string;
     revision: string;
