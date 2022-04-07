0003b0c7909dcb8aa1c9746ee3382ca2682a20ce
0xtaipoian <0xtaipoian@protonmail.com>
2022-04-07 18:52:01 +0800

    chore(depub.space): update geolocation channels

---
+++ packages/graphql/test/__mocks__/fixtures/notion-search-response.mock.ts
@@ -0,0 +1,197 @@
+export default {
+  object: 'list',
+  results: [
+    {
+      object: 'database',
+      id: '51650c66-938a-4916-935f-ce888d2ae692',
+      cover: null,
+      icon: null,
+      created_time: '2022-04-07T03:59:00.000Z',
+      created_by: {
+        object: 'user',
+        id: 'd3ce2f35-b534-46f0-902d-0db0b5849a10',
+      },
+      last_edited_by: {
+        object: 'user',
+        id: 'd3ce2f35-b534-46f0-902d-0db0b5849a10',
+      },
+      last_edited_time: '2022-04-07T07:43:00.000Z',
+      title: [
+        {
+          type: 'text',
+          text: {
+            content: 'Table: HK',
+            link: null,
+          },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: 'default',
+          },
+          plain_text: 'Table: HK',
+          href: null,
+        },
+      ],
+      properties: {
+        'List Name': {
+          id: '%3CYIo',
+          name: 'List Name',
+          type: 'select',
+          select: {
+            options: [
+              {
+                id: '698a009f-c3f2-4eb8-acc8-87d09ad40cb4',
+                name: 'Cosmos',
+                color: 'gray',
+              },
+              {
+                id: '32f950de-ebfd-4b4f-8f2d-51dbdccb4d14',
+                name: 'Featured',
+                color: 'orange',
+              },
+            ],
+          },
+        },
+        'Country Code': {
+          id: 'SKj%3C',
+          name: 'Country Code',
+          type: 'multi_select',
+          multi_select: {
+            options: [
+              {
+                id: 'anO;',
+                name: 'tw',
+                color: 'orange',
+              },
+              {
+                id: 't@\\\\',
+                name: 'hk',
+                color: 'purple',
+              },
+            ],
+          },
+        },
+        Order: {
+          id: 'oUMa',
+          name: 'Order',
+          type: 'number',
+          number: {
+            format: 'number',
+          },
+        },
+        'Channel(#)': {
+          id: 'title',
+          name: 'Channel(#)',
+          type: 'title',
+          title: {},
+        },
+      },
+      parent: {
+        type: 'page_id',
+        page_id: '807ca6ac-0851-4afc-9e7e-7c939f09d6d1',
+      },
+      url: 'https://www.notion.so/51650c66938a4916935fce888d2ae692',
+      archived: false,
+    },
+    {
+      object: 'database',
+      id: '85c0eaa3-e23f-46b8-b695-bf86fb30da7f',
+      cover: null,
+      icon: null,
+      created_time: '2022-03-09T23:06:00.000Z',
+      created_by: {
+        object: 'user',
+        id: 'd3ce2f35-b534-46f0-902d-0db0b5849a10',
+      },
+      last_edited_by: {
+        object: 'user',
+        id: 'd3ce2f35-b534-46f0-902d-0db0b5849a10',
+      },
+      last_edited_time: '2022-04-07T07:43:00.000Z',
+      title: [
+        {
+          type: 'text',
+          text: {
+            content: 'Table: Universal',
+            link: null,
+          },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: 'default',
+          },
+          plain_text: 'Table: Universal',
+          href: null,
+        },
+      ],
+      properties: {
+        'List Name': {
+          id: '%3CYIo',
+          name: 'List Name',
+          type: 'select',
+          select: {
+            options: [
+              {
+                id: '698a009f-c3f2-4eb8-acc8-87d09ad40cb4',
+                name: 'Cosmos',
+                color: 'gray',
+              },
+              {
+                id: '32f950de-ebfd-4b4f-8f2d-51dbdccb4d14',
+                name: 'Featured',
+                color: 'orange',
+              },
+            ],
+          },
+        },
+        'Country Code': {
+          id: 'SKj%3C',
+          name: 'Country Code',
+          type: 'multi_select',
+          multi_select: {
+            options: [
+              {
+                id: 'anO;',
+                name: 'tw',
+                color: 'orange',
+              },
+              {
+                id: 't@\\\\',
+                name: 'hk',
+                color: 'purple',
+              },
+            ],
+          },
+        },
+        Order: {
+          id: 'oUMa',
+          name: 'Order',
+          type: 'number',
+          number: {
+            format: 'number',
+          },
+        },
+        'Channel(#)': {
+          id: 'title',
+          name: 'Channel(#)',
+          type: 'title',
+          title: {},
+        },
+      },
+      parent: {
+        type: 'page_id',
+        page_id: '807ca6ac-0851-4afc-9e7e-7c939f09d6d1',
+      },
+      url: 'https://www.notion.so/85c0eaa3e23f46b8b695bf86fb30da7f',
+      archived: false,
+    },
+  ],
+  next_cursor: null,
+  has_more: false,
+};
