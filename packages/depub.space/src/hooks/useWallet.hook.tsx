7a8a9015a470f1e18f0b4c2521f1a041461ee1c2
William Chong <6198816+williamchong@users.noreply.github.com>
2022-05-06 19:02:12 +0800

    fix: update likecoin chain config

--- packages/depub.space/src/hooks/useWallet.hook.tsx
+++ packages/depub.space/src/hooks/useWallet.hook.tsx
@@ -109,12 +109,12 @@ export const getChainInfo = () => {
       coinType: 118,
     },
     bech32Config: {
-      bech32PrefixAccAddr: 'cosmos',
-      bech32PrefixAccPub: 'cosmospub',
-      bech32PrefixValAddr: 'cosmosvaloper',
-      bech32PrefixValPub: 'cosmosvaloperpub',
-      bech32PrefixConsAddr: 'cosmosvalcons',
-      bech32PrefixConsPub: 'cosmosvalconspub',
+      bech32PrefixAccAddr: 'like',
+      bech32PrefixAccPub: 'likepub',
+      bech32PrefixValAddr: 'likevaloper',
+      bech32PrefixValPub: 'likevaloperpub',
+      bech32PrefixConsAddr: 'likevalcons',
+      bech32PrefixConsPub: 'likevalconspub',
     },
     currencies: [
       {
@@ -138,7 +138,7 @@ export const getChainInfo = () => {
       average: 10,
       high: 1000,
     },
-    features: ['stargate', 'ibc-transfer'],
+    features: ['stargate', 'ibc-transfer', 'no-legacy-stdTx', 'ibc-go'],
   };
 
   const testnet = {
@@ -152,17 +152,17 @@ export const getChainInfo = () => {
       coinDecimals: 9,
       coinGeckoId: 'likecoin',
     },
-    walletUrlForStaking: 'https://stake.like.co',
+    walletUrlForStaking: 'https://likecoin-public-testnet-5.netlify.app',
     bip44: {
       coinType: 118,
     },
     bech32Config: {
-      bech32PrefixAccAddr: 'cosmos',
-      bech32PrefixAccPub: 'cosmospub',
-      bech32PrefixValAddr: 'cosmosvaloper',
-      bech32PrefixValPub: 'cosmosvaloperpub',
-      bech32PrefixConsAddr: 'cosmosvalcons',
-      bech32PrefixConsPub: 'cosmosvalconspub',
+      bech32PrefixAccAddr: 'like',
+      bech32PrefixAccPub: 'likepub',
+      bech32PrefixValAddr: 'likevaloper',
+      bech32PrefixValPub: 'likevaloperpub',
+      bech32PrefixConsAddr: 'likevalcons',
+      bech32PrefixConsPub: 'likevalconspub',
     },
     currencies: [
       {
@@ -182,8 +182,8 @@ export const getChainInfo = () => {
     ],
     coinType: 118,
     gasPriceStep: {
-      low: 0.01,
-      average: 1,
+      low: 1,
+      average: 10,
       high: 1000,
     },
   };
